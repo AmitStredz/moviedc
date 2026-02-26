e09857d62055db793a26a7ff5a245f9fd1cb3e3a
Ankitha-Sakthi <ankithasakthi@gmail.com>
2026-02-26 22:42:16 +0530

    Added gitignore

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+
 function Movies() {
+
+  const [movies, setMovies] = useState([]);
+
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
+  useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
+
+      .then(res => res.json())
+
+      .then(data => setMovies(data.results));
+
+  }, []);
+
   return (
+
     <div>
-      <h1>Movies Page</h1>
+
+      <h1>Now Playing Movies</h1>
+
+      {movies.map(movie => (
+
+        <p key={movie.id}>{movie.title}</p>
+
+      ))}
+
     </div>
+
   );
 }
 
