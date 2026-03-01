9abb0f757e9912c55f714989bd900773f1030ee6
ShadhaShareef <shadhashareef05@gmail.com>
2026-03-02 00:08:15 +0530

    Updated Movie Cards as a seperate component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 function Home() {
   const [movies, setMovies] = useState([]);
@@ -44,31 +45,7 @@ function Home() {
 
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
         {movies.map((movie) => (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <div className="bg-slate-800 p-3 rounded-xl shadow-md hover:scale-105 hover:shadow-xl transition transform cursor-pointer">
-
-              {movie.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                  className="rounded-lg mb-2"
-                />
-              ) : (
-                <div className="h-72 bg-slate-700 rounded-lg flex items-center justify-center">
-                  No Image
-                </div>
-              )}
-
-              <h3 className="text-sm font-semibold truncate text-white">
-                {movie.title}
-              </h3>
-
-              <p className="text-xs text-gray-300">
-                ⭐ {movie.vote_average}
-              </p>
-
-            </div>
-          </Link>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
@@ -83,31 +84,7 @@ function Movies() {
       {/* Movie Grid */}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
         {movies.map((movie) => (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <div className="bg-slate-800 p-3 rounded-xl shadow-md hover:scale-105 transition transform cursor-pointer">
-
-              {movie.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                  className="rounded-lg mb-2"
-                />
-              ) : (
-                <div className="h-72 flex items-center justify-center bg-slate-700 rounded-lg mb-2">
-                  No Image
-                </div>
-              )}
-
-              <h3 className="text-sm font-semibold truncate">
-                {movie.title}
-              </h3>
-
-              <p className="text-xs text-gray-300">
-                ⭐ {movie.vote_average}
-              </p>
-
-            </div>
-          </Link>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
