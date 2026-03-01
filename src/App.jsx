0c8737af856ab8ce7ef5116b3ebd468f34353ffb
abh1nav-pr <prabhinav2006@gmail.com>
2026-03-01 19:06:27 +0530

    Implemented Protected routes. Updated NavBar styling.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
 import MovieDetails from "./components/MovieDetails";
 import Profile from "./components/Profile";
-// import ProtectedRoute from "./components/ProtectedRoute";
+import { RedirectToSignIn } from "@clerk/clerk-react";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:id" element={<MovieDetails />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<RedirectToSignIn  redirectUrl={"/profile"}/>} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,38 +1,44 @@
 import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
+import Unauth from "./Unauth";
 
 export default function Profile() {
-    const { user } = useUser();
-    const [wishlist, setWishlist] = useState([]);
+  const { user } = useUser();
+  const [wishlist, setWishlist] = useState([]);
 
-    useEffect(() => {
-        if (!user) return;
+  useEffect(() => {
+    if (!user) return;
 
-        const stored = localStorage.getItem(`wishlist_${user.id}`);
+    const stored = localStorage.getItem(`wishlist_${user.id}`);
 
-        if (stored) {
-            setWishlist(JSON.parse(stored));
-        } else {
-            setWishlist([]);
-        }
+    if (stored) {
+      setWishlist(JSON.parse(stored));
+    } else {
+      setWishlist([]);
+    }
+  }, [user]);
 
-    }, [user]);
-
-    if (!user) return <p>Please sign in</p>;
+  if (!user)
+    return (
+      <div>
+        <Unauth />
+      </div>
+    );
 
   return (
     <div>
       <h1>{user.firstName}'s Wishlist</h1>
       <p>Total movies: {wishlist.length}</p>
-        {wishlist.length === 0 ? (
+      {wishlist.length === 0 ? (
         <p>No saved movies yet.</p>
-        ) : (wishlist.map((movie) => (
-            <div key={movie.id}>
+      ) : (
+        wishlist.map((movie) => (
+          <div key={movie.id}>
             <h3>{movie.title}</h3>
             <p>Rating: {movie.vote_average}</p>
-            </div>
+          </div>
         ))
-        )}
+      )}
     </div>
   );
 }
